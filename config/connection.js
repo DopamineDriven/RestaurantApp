cb37931fec4aa62446824f08988b51b2c870f702
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-01-28 02:27:25 -0600

    config folder , files, and pseudocode

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+//setting up mysql connection
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    //host: xyz.amazonaws.com or xyz.heroku goes here,
+    port: 3306,
+    user: 'root',
+    password: 'Wordpass3!!!992',
+    //database: database goes here
+});
+
+//establishing connetion
+
+connection.connect((error) => {
+    if (error) {
+        console.error(`error connecting ${error.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
