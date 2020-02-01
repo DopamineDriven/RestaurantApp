8eef973c67402ecabbd47916fac1743fbbe91175
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-01-31 21:33:33 -0600

    handlebars and handlers updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,13 +6,29 @@
 //})
 
 //preventDefault simplified using modal function
-window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(modal, 5000)
-});
+//window.addEventListener('DOMContentLoaded', () => {
+ // setTimeout(modal, 5000)
+//});
 
-async function modal () {
-  
-}
+//handling devour add event
+async function handleDevourAdd () {
+  try {
+    const id = this.dataset.id;
+    await fetch(`/api/burgers/${id}`, 
+    {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'
+    },
+    body: JSON.stringify({devoured: true})
+    });
+  }
+  catch (error) {
+    if (error) {
+      console.log(error)
+      throw error
+    }
+  }
+};
 
 async function handleDevour() {
   try {
@@ -31,3 +47,64 @@ async function handleDevour() {
     }
   }
 };
+
+function updatingAll() {
+  return new Promise ((resolve, reject) => {
+    //querySelectorAll returns all element descendants of node that match selectors
+    //devourList found in "views/devour.handlebars"
+    const devourList = document.querySelectorAll('.devour .card .card-button a');
+    for (let i=0; i<devourList.length; i++) {
+      try {
+        const id = devourList[i].dataset.id
+        const res = fetch(`/api/burgers/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({devoured: false})
+        });
+        //essentially, if the response is okay and i is equal to and of the same value as devourlist.length-1 (meaning one has been devoured) then resolve
+        if (res.ok && i === devourList.length-1) {
+          resolve()
+        }
+      }
+      catch (error) {
+        reject(error)
+      }
+    }
+  })
+};
+
+//handle devouring entire list (reference views/devour.handlebars) via updatingAll handler
+async function handleDevourAll() {
+  try {
+      await updatingAll();
+      window.location.replace('/devour')
+  } catch (error) {
+      if (error) {
+        console.log(error)
+        throw error
+      }
+  }
+};
+
+//handClick event
+async function handleClick () {
+  try {
+    const id = this.dataset.id;
+    await fetch(`/api/burgers/${id}`, 
+    {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'
+    },
+    body: JSON.stringify({devoured: true})
+    });
+  }
+  catch (error) {
+    if (error) {
+      console.log(error)
+      throw error
+    }
+  }
+};
+
