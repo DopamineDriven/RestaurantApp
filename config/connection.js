a8a64fbf1e92cf14e97b01dfdfc5af09be14384b
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-06 19:06:39 -0600

    updated connection to remote

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 //setting up mysql connection
 const mysql = require('mysql');
 let connection;
-console.log(process.env, process.env.JAWSDB_URL)
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {connection = mysql.createConnection({
-    //host: xyz.amazonaws.com or xyz.heroku goes here,
-    port: 3306,
-    user: 'root',
-    password: 'Wordpass3!!!992',
-    database: "burgers_db"
-})
+//heroku would not connect because JAWSDB_URL processing was undefined so key value defined here
+const JAWSDB_URL = 'mysql://i0tl3n1wicpz42dv:e5u43qa0wg7p66cr@j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ibum8812hgyphmpo'
+//I know that this is a security issue because the value contains username/password
+//console.log(process.env, process.env.JAWSDB_URL)
+if (JAWSDB_URL) {
+    connection = mysql.createConnection(JAWSDB_URL);
+// } else {connection = mysql.createConnection({
+//     //host: xyz.amazonaws.com or xyz.heroku goes here,
+//     port: 3306,
+//     user: 'root',
+//     password: 'Wordpass3!!!992',
+//     database: "burgers_db"
+// })
 };
 
 //establishing connection
