49d5088e8ed439a2177d90f274434219818615c1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-07 06:42:33 -0600

    fixes

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,14 @@
 const mysql = require('mysql');
 let connection;
 //importing key value pair from jaws.js
+//this is a necessary evil because after console logging JAWSDB_URL it was not defined when it should have been
+//I attempted to use a .gitignored jaws.js file to define this constant in but since it is gitignored heroku continued erroring out after the changes
+//I understand that this greatly compromises the security of the database and, in practice, would not settle for such a haphazard solution
 const JAWSDB_URL = 'mysql://i0tl3n1wicpz42dv:e5u43qa0wg7p66cr@j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ibum8812hgyphmpo'
 
 if (JAWSDB_URL) {
     connection = mysql.createConnection(JAWSDB_URL);
 } else {connection = mysql.createConnection({
-    //host: xyz.amazonaws.com or xyz.heroku goes here,
     port: 3306,
     user: 'root',
     password: 'Wordpass3!!!992',
