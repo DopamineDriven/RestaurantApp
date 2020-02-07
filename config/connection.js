e33f0d8d358d5cdd1ebfdd58e8b01b2e8639e0e0
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-07 03:35:38 -0600

    secured sensitive information via gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 //setting up mysql connection
 const mysql = require('mysql');
 let connection;
-//heroku would not connect because JAWSDB_URL processing was undefined so key value defined here
-const JAWSDB_URL = 'mysql://i0tl3n1wicpz42dv:e5u43qa0wg7p66cr@j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ibum8812hgyphmpo'
-//I know that this is a security issue because the value contains username/password
-//console.log(process.env, process.env.JAWSDB_URL)
+//importing key value pair from jaws.js
+const JAWSDB_URL = require('./jaws.js')
+
 if (JAWSDB_URL) {
     connection = mysql.createConnection(JAWSDB_URL);
 // } else {connection = mysql.createConnection({
