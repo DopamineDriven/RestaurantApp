81b3f78fd47dd4b205d5dd02fe6ad31eb1735c91
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-25 19:12:24 -0600

    push

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let connection;
 //this is a necessary evil because after console logging JAWSDB_URL it was not defined when it should have been
 //I attempted to use a .gitignored jaws.js file to define this constant in but since it is gitignored heroku continued erroring out after the changes
 //I understand that this greatly compromises the security of the database and, in practice, would not settle for such a haphazard solution
-const JAWSDB_URL = 'mysql://i0tl3n1wicpz42dv:e5u43qa0wg7p66cr@j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ibum8812hgyphmpo'
+const JAWSDB_URL = 'mysql://sevnxbbgockoooko:quteiqdar3wnm5v5@g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/f0kucnxr4wxz3mko'
 
 if (JAWSDB_URL) {
     connection = mysql.createConnection(JAWSDB_URL);
